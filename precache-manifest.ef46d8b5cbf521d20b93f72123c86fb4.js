bfea0871e97e21ceb3eea8aca353528cc53d17ce
Tim Vuegen <timvu@pop-os.localdomain>
2020-05-09 19:11:36 +0200

    Updates

---
+++ precache-manifest.ef46d8b5cbf521d20b93f72123c86fb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63212443135c16d99510954fbe3fc661",
+    "url": "/playlistgenerator/index.html"
+  },
+  {
+    "revision": "c711de1cc9f02425dec9",
+    "url": "/playlistgenerator/static/css/main.464a29e6.chunk.css"
+  },
+  {
+    "revision": "7a037d528ab24eae9c07",
+    "url": "/playlistgenerator/static/js/2.7a291611.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/playlistgenerator/static/js/2.7a291611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c711de1cc9f02425dec9",
+    "url": "/playlistgenerator/static/js/main.f2ef8ef6.chunk.js"
+  },
+  {
+    "revision": "52f8edcb3ad0c25e9320",
+    "url": "/playlistgenerator/static/js/runtime-main.81dcf764.js"
+  },
+  {
+    "revision": "6468634d7642b247f4a6247b94d05553",
+    "url": "/playlistgenerator/static/media/arrowleft.6468634d.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/playlistgenerator/static/media/cross.cad70081.svg"
+  }
+]);
